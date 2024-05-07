2c932c945dd74ab952314f0ebf481d26833411f5
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-05-07 06:44:59 +0000

    update

--- init.sh
+++ init.sh
@@ -11,7 +11,7 @@ if [ $(id -u) -ne 0 ]; then
   exit 1
 fi
 
-dnf -y install python3.11-pip git
+dnf -y install python3.12-pip git
 
 if [ -e /tmp/init ]; then
   mv -v /tmp/init /tmp/init.bak$$$(date +%Y%m%d%H%M%S)
@@ -19,9 +19,9 @@ fi
 
 git clone https://github.com/YumaYX/init.git /tmp/init
 cd /tmp/init \
-    && python3.11 -m venv venv \
+    && python3.12 -m venv venv \
     && source venv/bin/activate \
-    && python3.11 -m pip install -r requirements.txt \
+    && python3.12 -m pip install -r requirements.txt \
     && time ansible-playbook -i hosts r.yml -c local \
     && ansible-playbook -i hosts serverspec.yml -c local
 
