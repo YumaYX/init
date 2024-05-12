12269f4cdddd56d1e5cea821c99fe49e72797468
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-05-13 00:55:40 +0900

    update

--- serverspec/spec/localhost/ruby_spec.rb
+++ serverspec/spec/localhost/ruby_spec.rb
@@ -4,3 +4,12 @@ describe command('ruby -v') do
   its(:stdout) { should match /^ruby 3\.3\./ }
 end
 
+describe 'Check if gems are installed' do
+  gem_names = %w[rubocop minitest]
+
+  gem_names.each do |gem_name|
+    it "#{gem_name} should be installed" do
+      expect(`gem list --local`).to include(gem_name)
+    end
+  end
+end
---
+++ serverspec/spec/localhost/user_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe user('yuma') do
+  it { should exist }
+end
+
