abbe055f2b460f6aa8173842c10436a120f0344e
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-10-05 19:35:01 +0900

    update

--- init.sh
+++ init.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 
-if [ ! -f /etc/redhat-release ]; then
-    echo "terminated." 1>&2
-    exit 1
-fi
-
+test -f /etc/redhat-release || exit 1
 if [ $(id -u) -ne 0 ]; then
-  echo "run as root" 1>&2
-  echo "terminated." 1>&2
+  echo -e "run as root.\nterminated."
   exit 1
 fi
 
+
 dnf -y install python3-pip git make
 
-if [ -e /tmp/init ]; then
-  mv -v /tmp/init /tmp/init.$(date +%Y%m%d%H%M%S).${$}.0
-fi
-
+rm -rf /tmp/init
 git clone https://github.com/YumaYX/init.git /tmp/init
 cd /tmp/init && time make all
-
-mv -v /tmp/init /tmp/init.$(date +%Y%m%d%H%M%S).${$}.1
 
