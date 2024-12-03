768d49c2cb5d8d4ee67cf6e3135249a45fb9437a
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-12-04 01:13:01 +0900

    change

--- serverspec/spec/localhost/ruby_spec.rb
+++ serverspec/spec/localhost/ruby_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe command('sudo su - yuma -c "ruby -v"') do
+describe command('sudo su - user -c "ruby -v"') do
   its(:stdout) { should match /^ruby 3\.3\./ }
 end
 
@@ -9,11 +9,11 @@ describe 'Check if gems are installed' do
 
   gem_names.each do |gem_name|
     it "#{gem_name} should be installed" do
-      expect(`sudo su - yuma -c "gem list --local"`).to include(gem_name)
+      expect(`sudo su - user -c "gem list --local"`).to include(gem_name)
     end
   end
 end
 
 describe cron do
-  it { should have_entry('30 * * * * gem update --system && gem update').with_user('yuma') }
+  it { should have_entry('30 * * * * gem update --system && gem update').with_user('user') }
 end
