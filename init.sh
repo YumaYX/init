8a2426eca2a5a5b475820c1a1bd31c623427a934
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-08-05 06:38:04 -0400

    Install git and make

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ test -f /etc/redhat-release || exit 1
 test $(id -u) -eq 0 || exit 2
 
 dnf -y update
-dnf -y install python3-pip git make
+dnf -y install git make
 
 rm -rf /tmp/init
 git clone https://github.com/YumaYX/init.git /tmp/init
