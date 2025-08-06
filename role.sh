5f2befd9052682115e12b73986b31ba71375dc0e
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-08-06 07:58:46 -0400

    Update installation to use Ollama

--- role.sh
+++ role.sh
@@ -4,5 +4,5 @@ awk '{print $0; if ($0 ~ /roles:/)exit}' r.yml > .r.yml
 echo "    - ${1}" >> .r.yml
 
 make install
-source venv/bin/activate && ansible-playbook -v -i localhost, .r.yml -c local
+/root/.local/bin/uv run ansible-playbook -i localhost, -c local .r.yml -c local
 rm -f .r.yml
